e6ceed64dc4fccffc15edc03e0d3cd48ece21a72
JOSE ANTONIO COLLADO LOPEZ <7934546@ieseuropa.local>
2026-05-14 21:04:23 +0200

    Nuevo push de ejercicios nuevos

--- ActvidadesRepasoProgramacion/Actividad14/Program.cs
+++ ActvidadesRepasoProgramacion/Actividad14/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+var usuario = new Dictionary<int, string>();
+
+usuario[1] = "Uno";
+usuario[2] = "Dos";
+
+try
+{
+    string nombre = usuario[99];
+
+}
+catch (KeyNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+if (usuario.TryGetValue(99, out string nombre))
+{
+    Console.WriteLine(nombre);
+}
+
+
+
