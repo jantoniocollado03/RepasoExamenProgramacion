8931ec5ce79aa002be5204851be88ff835dd1626
JOSE ANTONIO COLLADO LOPEZ <7934546@ieseuropa.local>
2026-05-14 21:15:02 +0200

    Nuevo push de ejercicios nuevos

---
+++ ActvidadesRepasoProgramacion/Actividad15/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var alumno = new List<Alumno>();
+
+alumno.Add(new Alumno{Nombre = "Frasquito", Clase = "1A", NotaMedia = 5});
+alumno.Add(new Alumno{Nombre = "Julián", Clase = "1B", NotaMedia = 3});
+
+var todos = alumno.All(x=>x.NotaMedia>=5);
+
+var matricula = alumno.All(x=>x.NotaMedia==10);
+
+var grupos = alumno.GroupBy(x=>x.Clase).ToList();
+
+
+public class Alumno
+{
+    public string Nombre { get; set; }
+    public string Clase { get; set; }
+    public double NotaMedia { get; set; }
+}
